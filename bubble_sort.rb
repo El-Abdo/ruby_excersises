a4de0201320ddc8f899801550fa13c2fa13b5283
El-abdo <abdulrahman.muhammad2090@gmail.com>
2022-12-30 21:10:58 +0200

    distinct numbers only

---
+++ bubble_sort.rb
@@ -0,0 +1,31 @@
+def sorted (array)
+    array.each_with_index do |element, index|  
+        next if index == 0
+        if element < array[index - 1]
+            return false
+        end
+    end
+    return true
+end
+
+def switch (array,element)
+    idx = array.find_index(element) #causes bug when repeated number because it fetches the first element
+    array.insert(idx -1, array.delete_at(idx))
+    
+end
+
+def bubble_sort (array)
+    while (not sorted(array)) == true 
+        array.each_with_index do |element, index|
+            next if index == 0
+            if element < array[index - 1]
+                switch(array,element)
+            end
+               
+        end
+    end
+    puts array
+    
+end
+
+puts switch([1,1,3,2],1)
